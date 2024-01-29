acb652df2a8ad0cc163f94447000f44bf938438b
bbcvasconcellos <bbcvasconcellos@gmail.com>
2024-01-29 05:58:04 -0300

    add: cookie.svg, modal component & new custom styles in tailwind config

--- src/components/cookiesModal.tsx
+++ src/components/cookiesModal.tsx
@@ -1,3 +1,54 @@
+import { useState } from "react"
+import Image from "next/image"
+import { Button } from "./button"
+
+import cookie from "../../public/cookie.svg"
+import quintera_logo from "../../public/quintera_logo.svg"
+
 export const CookiesModal = () => {
-  
+  const [isOpen, setIsOpen] = useState(true);
+  return (
+    <>
+    { isOpen ? 
+      <div className="bg-black">
+        <div className="w-screen flex px-16 py-14 gap-12 justify-between bg-white">
+          <div className="flex">
+            <Image src={cookie} alt="cookie" />
+            <div className="max-w-lg flex flex-col justify-end gap-8">
+              <Image src={quintera_logo} alt="quintera logo" />
+              <div className="flex flex-col gap-4">
+                <h3 className="font-semibold text-2xl">Política de cookies</h3>
+                <p className="text-sm">
+                  Para otimizar a experiência durante a sua navegação em nosso site, fazemos uso de cookies. 
+                  Ao continuar na nossa plataforma, 
+                  consideramos que você esteja de acordo com nossa política de cookies.”
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="flex gap-2 items-end">
+            <Button 
+              text="Recusar"
+              style={{ 
+                backgroundColor: "white",
+                color: "#344054",
+                border: "1px #344054 solid",
+                width: "182px"
+              }}
+              onClick={() => setIsOpen(prev => !prev)}
+            />
+            <Button 
+              text="Aceitar"
+              style={{ 
+                width: "182px"
+              }}  
+              onClick={() => setIsOpen(prev => !prev)}
+            />
+          </div>
+        </div>
+      </div> : 
+      <></>
+    }
+    </>
+  )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,11 +22,14 @@ const config: Config = {
         line: "#E9E9E9",
         orbit: "#CDCDCD",
         planet: "#D9D9D9",
+        smoke: "rgba(0,0,0,0.5)",
+        "gray-100": "#F2F2F2",
         "gray-200": "#EAECF0",
         "gray-300": "#E0DDDD",
         "gray-400": "#D0D5DD",
         "gray-500": "#4F4C4C",
         "gray-600": "#667085",
+        "gray-700": "#D6D6D6",
       },
       fontSize: {
         header: "2.5rem",
@@ -35,6 +38,22 @@ const config: Config = {
         "spin-xs": "spin 20s linear infinite",
         "spin-s": "spin 15s linear infinite",
         "spin-normal": "spin 12s linear infinite",
+        "rotation-balance": "balance 6s linear infinite",
+        rotateY: "rotateAnimation 12s steps(1) infinite",
+      },
+      keyframes: {
+        balance: {
+          from: {
+            transform: "rotate(0deg)",
+          },
+          to: {
+            transform: "rotate(-180deg)",
+          },
+        },
+        rotateAnimation: {
+          "0%": { transform: "rotate(0deg)" },
+          "50%": { transform: "rotate(-180deg)" },
+        },
       },
     },
   },
