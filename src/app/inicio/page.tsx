17b17013e1c5a9c40cd1799965bac2c3899bb523
bbcvasconcellos <bbcvasconcellos@gmail.com>
2024-02-02 23:45:18 -0300

    update: page text, layout elements add: supporttext with all the clients requested texts

--- src/app/inicio/page.tsx
+++ src/app/inicio/page.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 
 import Image from "next/image";
+import dynamic from "next/dynamic";
 
 import { TbTargetArrow } from "react-icons/tb";
 import { FaArrowTrendUp } from "react-icons/fa6";
@@ -16,20 +17,25 @@ import { HiOutlineChip } from "react-icons/hi";
 import { HiOutlineSquare3Stack3D } from "react-icons/hi2";
 import { PiGitFork } from "react-icons/pi";
 
-import { Tag } from "@/components/tag";
+// import { Tag } from "@/components/tag";
 import { Card } from "@/components/card";
 import { Input } from "@/components/input";
-import { Orbit } from "@/components/orbit";
+import { Loading } from "@/components/loading";
+//import { Orbit } from "@/components/orbit";
+const Orbit = dynamic(() => import("../../components/orbit"), { ssr: false, loading: () => <Loading />
+})
 import { Button } from "@/components/button";
 import { Highlight } from "@/components/highlight";
 // import { CookiesModal } from "@/components/cookiesModal";
 import { MobileNavbar } from "@/components/mobileNavbar";
-import { WhatsAppModal } from "@/components/whatsappModal";
+//import { WhatsAppModal } from "@/components/whatsappModal";
+const WhatsAppModal = dynamic(() => import("../../components/whatsappModal"), { ssr: false, loading: () =>  <Loading />})
 
-import globe from "../../../public/globe.svg";
+// import globe from "../../../public/globe.svg";
 import email from "../../../public/email.svg";
 import brazil from "../../../public/brazil.svg";
 import quintera_logo from "../../../public/quintera_logo.svg";
+import { supportText } from "./supportText";
 
 export default function Intro() {
   const [isOpen, setIsOpen] = useState(false);  
@@ -39,7 +45,7 @@ export default function Intro() {
       {/* <CookiesModal /> */}
       { isOpen ? <WhatsAppModal isOpen={isOpen} setIsOpen={setIsOpen}/> : <></>}
       <MobileNavbar />
-      <section className="w-screen bg-main lg:px-[4.5rem] px-6 md:pt-[calc(5rem+78px)] pt-[calc(50px+1.5rem)] flex flex-col">
+      <section className="w-screen bg-main lg:px-[4.5rem] px-6 md:pt-[calc(5rem+78px)] pt-[calc(50px+1.5rem)] flex flex-col md:pb-28">
         <div className="md:grid md:grid-cols-2 gap-12 place-self-center"> {/*review gap-12 -> lower it?*/}
           {/* textos */}
           <div className="max-w-xl flex flex-col gap-6 place-items-center md:place-items-start">
@@ -52,11 +58,11 @@ export default function Intro() {
             </div>
             <div className="w-fit flex flex-col gap-2 md:gap-6 place-items-center md:place-items-start">
               <h1 className="text-secondary font-roboto md:font-black font-bold md:text-left text-center md:text-header max-md:max-w-64 text-lg md:leading-[3rem] leading-6">
-                Referência para o setor público em soluções personalizadas na segurança de dados
+                {supportText.home.title}
               </h1>
-              <span className="md:text-subtitle text-gray-500 font-normal font-inter text-md md:text-left text-center leading-4 md:max-w-none max-w-72">
-                Nossa missão é arrumar sua casa com excelência para estar em conformidade com as mais atuais leis de segurança de dados. 
-              </span>
+              <h2 className="md:text-subtitle text-gray-500 font-normal font-inter text-md md:text-left text-center leading-4 md:max-w-none max-w-72">
+                {supportText.home.subtitle}
+              </h2>
             </div>
           </div>
           {/* animacao */}
@@ -64,7 +70,7 @@ export default function Intro() {
             <Orbit />
           </div>
         </div>
-        <div className="flex md:flex-row flex-col justify-center items-center md:w-full w-[calc(16rem+1.5rem)] self-center md:gap-6 gap-3 md:h-32 md:px-48 px-5 md:py-8 py-5 md:mt-28 mt-6 md:mb-12 mb-8 rounded-2xl border-white border shadow-md shadow-black/10">
+        {/* <div className="flex md:flex-row flex-col justify-center items-center md:w-full w-[calc(16rem+1.5rem)] self-center md:gap-6 gap-3 md:h-32 md:px-48 px-5 md:py-8 py-5 md:mt-28 mt-6 md:mb-12 mb-8 rounded-2xl border-white border shadow-md shadow-black/10">
           <h4 className="flex md:flex-row flex-col items-center font-inter font-medium text-gray-500 md:text-base text-xs md:leading-5 leading-3 md:text-left text-center">
             <span className="md:text-header text-3xl font-black text-primary md:mr-2 font-roboto">1046</span>
             PROJETOS<br/> EXECUTADOS
@@ -79,17 +85,17 @@ export default function Intro() {
             <span className="md:text-header text-3xl font-black text-primary md:mr-2 font-roboto">100%</span>
             PROJETOS<br/> ENTREGUES
           </h4>
-        </div>
+        </div> */}
       </section>
       <section id="beneficios" className="w-screen bg-main lg:px-44 px-6 md:pb-16 pb-7 flex flex-col md:gap-8 gap-2 items-center justify-center">
         <div className="flex items-center justify-center">
           <Highlight 
             icon={<FaArrowTrendUp />}
-            description="Benefícios exclusivos"
+            description={supportText.benefits.highlight}
           />
         </div>
         <h2 className="md:font-black font-bold font-roboto md:text-header md:leading-10 text-lg text-center md:max-w-none max-w-52 text-primary">
-          Por que a Quintera se destaca no mercado?
+          {supportText.benefits.title}
         </h2>
         <div className="md:hidden max-w-64 overflow-visible flex flex-col gap-2 mb-[calc(0.5rem+1rem)]">
           <div className="flex justify-center gap-2">
@@ -103,83 +109,67 @@ export default function Intro() {
           </div>
         </div>
         <div className="md:grid md:grid-cols-2 md:gap-[4.5rem] flex flex-col gap-6">
-          <div className="md:w-96 md:p-10 p-7 flex flex-col items-center rounded-2xl border border-gray-300">
-            <Image src={quintera_logo} alt="quinterati logo" className="ml-auto mr-auto"/>
-            <summary className="list-none md:mt-2 mt-4 md:mb-5 mb-8 text-center md:text-left text-gray-500 font-inter font-medium text-md leading-5">
-              Nós exergamos a necessidade do 
-              setor público por parceiros realmente
-              preocupados com a entrega e sustentação
-              de seus produtos.
+          <div className="md:w-96 p-7 md:px-10 md:py-7 flex flex-col max-sm:items-center rounded-2xl border border-gray-300">
+            <Image src={quintera_logo} alt="quinterati logo"/>
+            <summary className="list-none md:mt-3 mt-4 md:mb-4 mb-8 text-center md:text-left text-gray-500 font-inter font-medium text-md leading-5">
+             {supportText.benefits.summary}
             </summary>
             <dl className="flex flex-col gap-2 text-gray-500 font-inter font-medium text-md mr-auto">
-              <dt className="flex gap-1">
-                <GoCheckCircle size={24} color="#001E3B"/>
-                <span>Consultoria e entendimento</span>
+              {supportText.benefits.topics.map((topic, index) => (
+                <dt className="flex gap-1" key={index}>
+                  <GoCheckCircle size={24} color="#001E3B"/>
+                  <span>{topic}</span>
               </dt>
-              <dt className="flex gap-1">
-                <GoCheckCircle size={24} color="#001E3B"/>
-                <span>Solução personalizada</span>
-              </dt>
-              <dt className="flex gap-1">
-                <GoCheckCircle size={24} color="#001E3B"/>
-                <span>Pagamento em reais</span>
-              </dt>
-              <dt className="flex gap-1">
-                <GoCheckCircle size={24} color="#001E3B"/>
-                <span>Suporte que funciona</span>
-              </dt>
-              <dt className="flex gap-1">
-                <GoCheckCircle size={24} color="#001E3B"/>
-                <span>Acompanhamento da implantação</span>
-              </dt>
+              ))}
             </dl>
-          </div>
-          <div className="relative flex flex-col items-center"> 
-            <Image src={brazil} alt="mapa do Brasil"/>
-            <div className="md:absolute lg:-right-20 lg:bottom-5 md:bottom-2 mt-6">
+            <div className="mt-5">
               <Button 
                 text="Entrar em contato"
                 icon={<FaWhatsapp />}
                 onClick={() => setIsOpen(prevState => !prevState)}
               />
             </div>
-            <div className="absolute lg:-right-24 lg:bottom-20 bottom-1/2">
+          </div>
+          <div className="relative flex flex-col items-center"> 
+            <Image src={brazil} alt="mapa do Brasil"/>
+            <div className="md:absolute lg:-right-20 lg:bottom-5 md:bottom-2 mt-6">
+              
+            </div>
+            {/* <div className="absolute lg:-right-24 lg:bottom-20 bottom-1/2">
               <Tag 
                 icon={globe}
                 description="Em todo o Brasil você
                 encontra a excelênica
                 da Quintera!"
               />
-            </div>
+            </div> */}
           </div>
         </div>
       </section>
       <section className="w-screen md:bg-white bg-gray-700 md:px-28 px-6 md:pt-12 py-8 md:pb-4 flex flex-col gap-12 items-center">
         <div className="flex flex-col items-center justify-center gap-4 md:py-6 md:bg-main md:border w-full md:border-gray-300 md:rounded-2xl md:shadow-md md:shadow-black-[.1]">
           <Highlight 
-            description="Fortalecimento do setor público"
+            description={supportText.box.hightlight}
             icon={<BsTrophy />}
             bgColor="blue"
           />
-          <div className="flex flex-col gap-2 justify-center items-center">
+          <div className="flex flex-col gap-4 max-sm:gap-2 justify-center items-center">
             <h3 className="font-black font-roboto text-header text-center text-support max-w-72 md:max-w-none leading-9">
-              Exclusividade EMX no Brasil
+              {supportText.box.title}
             </h3>
-            <summary className="list-none font-inter font-medium text-center text-md md:max-w-md text-support leading-5 max-w-80">
-              Trabalhar em parceria com empresas líderes de mercado facilita o compartilhamento de ideias e experiências, 
-              resultando em soluções mais eficientes e 
-              adaptadas as necessidades do mercado além de um enorme compromisso com a excelência em suas entregas.
+            <summary className="list-none font-inter font-medium text-center text-md md:max-w-3xl text-support leading-5 max-w-80">
+              {supportText.box.summary}
             </summary>
           </div>
         </div>
       </section>
       <section id="contato" className="w-screen bg-white flex flex-col md:pb-4 md:pt-8 pt-6 items-center md:px-36 px-8">
         <Highlight 
-            description="Contato do especialista"
+            description={supportText.contact.highligh}
             icon={<TbTargetArrow />}
           />
         <h3 className="font-black font-roboto md:text-header text-lg leading-6 md:leading-10 text-center text-primary max-md:max-w-64 md:mb-12 md:mt-4 mt-2 mb-3">
-          Precisa de um contato personalizado?
+          {supportText.contact.title}
         </h3>
         <div className="lg:grid lg:grid-cols-2 lg:gap-9 flex flex-col-reverse ">
           <form className="flex flex-col gap-4 px-3">
@@ -227,43 +217,22 @@ export default function Intro() {
           icon={<TbTargetArrow />}
         />
         <h3 className="font-black font-roboto md:text-header text-lg max-md:max-w-64 leading-6 md:leading-10 max-md:mt-2 max-md:mb-6 text-center text-primary">
-          Veja algumas perguntas frequentes sobre:
+          {supportText.faq.title}
         </h3>
         <div className="md:grid md:grid-cols-3 md:gap-x-16 md:gap-y-8 flex flex-col max-md:w-full max-md:gap-2">
           <Card 
-            header="Documentação" 
-            content="O planejamento financeiro é uma peça fundamental no quebra-cabeça do sucesso empresarial, 
-            e na advocacia não é diferente." 
-            icon={<TiDocumentText />} 
-          />
-          <Card 
-            header="LGPD" 
-            content="O planejamento financeiro é uma peça fundamental no quebra-cabeça do sucesso empresarial, 
-            e na advocacia não é diferente." 
+            header={supportText.faq.topics[0].header}
+            content={supportText.faq.topics[0].content}
             icon={<RiFolderLockLine />} 
           />
           <Card 
-            header="Cibersegurança"
-            content="O planejamento financeiro é uma peça fundamental no quebra-cabeça do sucesso empresarial,
-             e na advocacia não é diferente."  
+            header={supportText.faq.topics[1].header}
+            content={supportText.faq.topics[1].content}
             icon={<FiLock />}
           />
           <Card 
-            header="TI"
-            content="O planejamento financeiro é uma peça fundamental no quebra-cabeça do sucesso empresarial,
-             e na advocacia não é diferente."  
-            icon={<HiOutlineChip />} 
-          />
-          <Card 
-            header="Serviços"
-            content="O planejamento financeiro é uma peça fundamental no quebra-cabeça do sucesso empresarial,
-             e na advocacia não é diferente."  
-            icon={<HiOutlineSquare3Stack3D />} 
-          />
-          <Card 
-            header="Infraestrutura"
-            content="O planejamento financeiro é uma peça fundamental no quebra-cabeça do sucesso empresarial,
-             e na advocacia não é diferente."  
+            header={supportText.faq.topics[2].header}
+            content={supportText.faq.topics[2].content}
             icon={<PiGitFork style={{ transform: "rotate(90deg)" }}/>}
           />
         </div>
---
+++ src/app/inicio/supportText.tsx
@@ -0,0 +1,46 @@
+export const supportText = {
+  home: {
+    title: "Projetos customizados e escaláveis para superar desafios como, conformidade com a LGPD, Cibersegurança e Infraestrutura de Datacenter",
+    subtitle: "Nossa missão é superar as expectativas do cliente com as melhores experiências de compra e ser reconhecido pelo compromisso e qualidade da entrega."
+  },
+  benefits: {
+    highlight: "Benefícios exclusivos",
+    title: "Por que a Quintera se destaca no mercado?",
+    summary: "Nós enxergamos a necessidade do cliente e apoiando na melhor estratégia",
+    topics: [
+      "Análise consultiva",
+      "Soluções sob medida",
+      "Projetos End-to-End",
+      "Continuidade e Melhoria dos processos",
+      "Presença nacional",
+    ]
+  },
+  box: {
+    hightlight: "Adequação, controle, gestão e compliance com a LGPD",
+    title: "Única, simples e eficiente",
+    summary: "Fortalecimento das organizações para se adequar às leis e normas regulatórias com as melhores práticas para o controle e gestão para a conformidade com a LGPD, acompanhado por especialistas e uma equipe multidisciplinar para garantir o sucesso da implantação do processo."
+  },
+  contact: {
+    highligh: "Contato do especialista",
+    title: "Precisa de um contato personalizado?",
+  },
+  faq: {
+    highlight: "Sessão de ajuda por soluções",
+    title: "Veja algumas perguntas frequentes sobre:",
+    topics: [
+      {
+        header: "LGPD",
+        content: "A LGPD é a legislação brasileira que regula o tratamento de dados pessoais, visando proteger a privacidade e os direitos dos cidadãos."
+      }, 
+      {
+        header: "Cibersegurança",
+        content: "Cibersegurança refere-se às práticas e medidas adotadas para proteger sistemas, redes e dados contra ameaças cibernéticas."
+      },
+      {
+        header: "Infraestrutura",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad"
+      }
+    ]
+  }
+
+}
