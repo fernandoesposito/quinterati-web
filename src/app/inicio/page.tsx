c4dd9f7f97ecee9e62c02c2434c5693737b53ec8
Thiago Vasconcellos <thiagovasconcellos88@gmail.com>
2024-04-05 00:50:05 +0100

    added cookies feature to right place

--- src/app/inicio/page.tsx
+++ src/app/inicio/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { FormEvent, useRef, useState } from "react";
-import Head from 'next/head';
 
 import Link from "next/link";
 import Image from "next/image";
@@ -97,10 +96,6 @@ export default function Intro() {
 
   return (
     <>
-      <Head>
-        <meta name="emx-lgpsite-id" content="1" />
-        <script src="https://lgpdquintera.nuvemx.com.br/Cookies/emx-inject.js?website_code=1" className="emx-cookie" async></script>
-      </Head>
       <MobileNavbar />
       <EmailModal isOpen={isOpen} setIsOpen={setIsOpen} />
       <WhatsAppModal
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+import Head from 'next/head';
+
 import Intro from "./inicio/page";
 
 export default function Home() {
   return (
     <main>
+      <Head>
+        <meta name="emx-lgpsite-id" content="1" />
+        <script src="https://lgpdquintera.nuvemx.com.br/Cookies/emx-inject.js?website_code=1" className="emx-cookie" async></script>
+      </Head>
       <Intro />
     </main>
   );
