b612c3397161808f4805f6ff4d3f9428e77774f9
fernandoesposito <159202130+fernandoesposito@users.noreply.github.com>
2026-09-09 14:52:42 -0300

    correcao page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
-import Head from 'next/head';
-
 import Intro from "./inicio/page";
 
 export default function Home() {
   return (
     <main>
-      <Head>
-        {/* <meta name="emx-lgpsite-id" content="1" />
-        <script src="https://lgpdquintera.nuvemx.com.br/Cookies/emx-inject.js?website_code=1" className="emx-cookie" async></script> */}
-      </Head>
       <Intro />
     </main>
   );
